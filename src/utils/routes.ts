1e9653e6367d5e9f6dbdaa31be830241c911e8e0
david-sds <dave.sathler@gmail.com>
2023-06-18 17:09:26 -0300

    fix route and deleteconfirmation

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,10 +2,12 @@ import * as VueRouter from 'vue-router'
 
 import FinishSignupComponent from '@/pages/FinishSignup.vue'
 import HomeComponent from '@/pages/Home.vue'
+import LandingComponent from '@/pages/Landing.vue'
+import Login from '@/pages/Login.vue'
 
 const routes: any = [
   { path: '/', name: 'landing', component: LandingComponent },
-  { path: '/login', name: 'login', component: HomeComponent },
+  { path: '/login', name: 'login', component: Login },
   { path: '/finish-signup', name: 'signup', component: FinishSignupComponent },
   {
     path: '/home',
